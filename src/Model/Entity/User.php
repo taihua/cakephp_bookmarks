07632df87ffb3e8e1bfe08a214fd992106e85ed9
taihua <chen@renosys.co.jp>
2017-06-16 00:47:02 -0700

    Adding Password Hashing

--- src/Model/Entity/User.php
+++ src/Model/Entity/User.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Model\Entity;
 
+use Cake\Auth\DefaultPasswordHasher; // この行を追加してください
 use Cake\ORM\Entity;
 
 /**
@@ -39,4 +40,11 @@ class User extends Entity
     protected $_hidden = [
         'password'
     ];
+
+    protected function _setPassword($value)
+    {
+        $hasher = new DefaultPasswordHasher();
+        return $hasher->hash($value);
+    }
+
 }
