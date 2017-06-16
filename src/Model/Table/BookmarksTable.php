b17024b72cb4288ae85cc2252a944ada774f3cff
taihua <chen@renosys.co.jp>
2017-06-16 00:53:14 -0700

    Getting Bookmarks with a Specific Tag
    Ideally, we’d have a URL that looks like http://localhost:8765/bookmarks/tagged/funny/cat/gifs. This would let us find all the bookmarks that have the ‘funny’, ‘cat’ or ‘gifs’ tags.

--- src/Model/Table/BookmarksTable.php
+++ src/Model/Table/BookmarksTable.php
@@ -89,4 +89,26 @@ class BookmarksTable extends Table
 
         return $rules;
     }
+
+    // The $query argument is a query builder instance.
+    // The $options array will contain the 'tags' option we passed
+    // to find('tagged') in our controller action.
+    public function findTagged(Query $query, array $options)
+    {
+        $bookmarks = $this->find()
+            ->select(['id', 'url', 'title', 'description']);
+
+        if (empty($options['tags'])) {
+            $bookmarks
+                ->leftJoinWith('Tags')
+                ->where(['Tags.title IS' => null]);
+        } else {
+            $bookmarks
+                ->innerJoinWith('Tags')
+                ->where(['Tags.title IN ' => $options['tags']]);
+        }
+
+        return $bookmarks->group(['Bookmarks.id']);
+    }
+
 }
